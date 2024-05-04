9b9201f862fb9b14731a858ddfcc6d930d1a4018
Tuviccfp <victorcordeirofp@gmail.com>
2024-05-04 15:29:42 -0300

    inclusão de parâmetro no map de ListCategory Generic

--- src/components/ListCategory/index.js
+++ src/components/ListCategory/index.js
@@ -19,8 +19,8 @@ export default function ListCategoryComponent({props}) {
     return (
         <div>
             <select value={navigateId} onChange={handleChange}>
-                {props.map(() => (
-                    <option key={props.id} value={props.id}>{props.name}</option>
+                {props.map((item) => (
+                    <option key={item.id} value={item.id}>{item.categoryName}</option>
                 ))}
             </select>
             <button type="submit" onClick={handleNavigate}>Enviar</button>
